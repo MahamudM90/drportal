6884287690ca117aab4e2a1918e247e0f6ecc93c
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2023-01-27 22:16:40 +0600

    hero portion

--- client/src/Routes/Routes/Routes.js
+++ client/src/Routes/Routes/Routes.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
 import Home from "../.././Pages//Home//Home//Home";
-import Login from "../../././Pages///Login/Login"
+import Login from "../../././Pages///Login/Login";
 const router = createBrowserRouter([
   {
     path: "/",
